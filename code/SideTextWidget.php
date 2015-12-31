f3529f67156c26f5ea22f52b992f5e8b8cadbc69
helpfulrobot <chris@silverstripe.com>
2015-12-31 23:56:24 +1300

    Converted to PSR-2

--- code/SideTextWidget.php
+++ code/SideTextWidget.php
@@ -1,39 +1,45 @@
 <?php
-class SideTextWidget extends Widget {
+class SideTextWidget extends Widget
+{
 
-	static $has_one = array(
-		"SideTextWidgetContent" => "SideTextWidgetDataObject"
-	);
+    public static $has_one = array(
+        "SideTextWidgetContent" => "SideTextWidgetDataObject"
+    );
 
-	static $title       = "Side bar section";
+    public static $title       = "Side bar section";
 
-	static $cmsTitle    = "Side bar section";
+    public static $cmsTitle    = "Side bar section";
 
-	static $description = "Add a section to your side bar";
+    public static $description = "Add a section to your side bar";
 
-	function SideText(){
-		return DataObject::get_by_id("SideTextWidgetDataObject", $this->SideTextWidgetContentID);
-	}
+    public function SideText()
+    {
+        return DataObject::get_by_id("SideTextWidgetDataObject", $this->SideTextWidgetContentID);
+    }
 
-	function getCMSFields(){
-		$source = DataObject::get("SideTextWidgetDataObject");
-		if($source && $source->count()) {
-			$list = $source->map("ID", "Title");
-			$listField = new DropdownField( "SideTextWidgetContentID", _t('SideTextWidget.CHOOSE','Side Text Widget'), $list);
-			return new FieldList($listField);
-		}
-	}
+    public function getCMSFields()
+    {
+        $source = DataObject::get("SideTextWidgetDataObject");
+        if ($source && $source->count()) {
+            $list = $source->map("ID", "Title");
+            $listField = new DropdownField("SideTextWidgetContentID", _t('SideTextWidget.CHOOSE', 'Side Text Widget'), $list);
+            return new FieldList($listField);
+        }
+    }
 
-	function getCmsTitle() {
-		return _t('SideTextWidget.CMSTITLE', $this->stat('cmsTitle'));
-	}
+    public function getCmsTitle()
+    {
+        return _t('SideTextWidget.CMSTITLE', $this->stat('cmsTitle'));
+    }
 }
 
-class SideTextWidget_CMSHack extends LeftAndMainExtension {
-	function init(){
-		HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats','p,h1');
-		HtmlEditorConfig::get('cms')->setButtonsForLine(1,'undo, redo, separator, cut, copy, pastetext, separator, ssimage, sslink, unlink, separator, fullscreen, advcode, formatselect');
-		HtmlEditorConfig::get('cms')->setButtonsForLine(2);
-		HtmlEditorConfig::get('cms')->setButtonsForLine(3);
-	}
+class SideTextWidget_CMSHack extends LeftAndMainExtension
+{
+    public function init()
+    {
+        HtmlEditorConfig::get('cms')->setOption('theme_advanced_blockformats', 'p,h1');
+        HtmlEditorConfig::get('cms')->setButtonsForLine(1, 'undo, redo, separator, cut, copy, pastetext, separator, ssimage, sslink, unlink, separator, fullscreen, advcode, formatselect');
+        HtmlEditorConfig::get('cms')->setButtonsForLine(2);
+        HtmlEditorConfig::get('cms')->setButtonsForLine(3);
+    }
 }
